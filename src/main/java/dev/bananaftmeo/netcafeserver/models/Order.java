41b14a2a6d3e11b03e6be90e71ad8f7492bc0fb6
bananaback <58079308+bananaback@users.noreply.github.com>
2024-05-07 14:42:18 +0700

    Implement order item CRUD.

--- src/main/java/dev/bananaftmeo/netcafeserver/models/Order.java
+++ src/main/java/dev/bananaftmeo/netcafeserver/models/Order.java
@@ -1,6 +1,7 @@
 package dev.bananaftmeo.netcafeserver.models;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
 import dev.bananaftmeo.netcafeserver.enums.OrderStatusEnum;
 import jakarta.persistence.Column;
@@ -12,6 +13,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -27,6 +29,7 @@ import lombok.Setter;
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
     private Long id;
 
     @Column(nullable = false)
@@ -37,4 +40,7 @@ public class Order {
 
     @ManyToOne(fetch = FetchType.LAZY)
     ApplicationUser user;
+
+    @OneToMany(mappedBy = "order")
+    Set<OrderItem> orderItems;
 }
---
+++ src/main/java/dev/bananaftmeo/netcafeserver/repositories/OrderItemRepository.java
@@ -0,0 +1,25 @@
+package dev.bananaftmeo.netcafeserver.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import dev.bananaftmeo.netcafeserver.models.OrderItem;
+import dev.bananaftmeo.netcafeserver.models.OrderItemId;
+import dev.bananaftmeo.netcafeserver.models.dtos.OrderItemDTO;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemId> {
+    @Query("SELECT new dev.bananaftmeo.netcafeserver.models.dtos.OrderItemDTO(oi.quantity, p.price, oi.product.id, oi.order.id) "
+            +
+            "FROM OrderItem oi " +
+            "JOIN oi.product p " +
+            "WHERE oi.product.id = :productId AND oi.order.id = :orderId")
+    OrderItemDTO findOrderItemDTOByProductIdAndOrderId(Long productId, Long orderId);
+
+    @Query("SELECT new dev.bananaftmeo.netcafeserver.models.dtos.OrderItemDTO(oi.quantity, p.price, oi.product.id, oi.order.id) "
+            +
+            "FROM OrderItem oi " +
+            "JOIN oi.product p")
+    List<OrderItemDTO> findAllOrderItemDTOs();
+}
